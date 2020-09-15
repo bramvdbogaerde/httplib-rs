8fe84fc7c2fb44ae5c8d2efffd8a94ce94a481f3
Bram Vandenbogaerde <contact@bramvdb.com>
2020-09-15 17:35:47 +0200

    init

---
+++ README.md
@@ -0,0 +1,11 @@
+HTTPLib
+========
+
+HTTPLib is an opinionated HTTP client library written in Rust that uses
+the `surf` library in combination with Serde. 
+
+It has been written out of the observation that I was writing 
+very similar code for each project that involved an HTTP client.
+
+The library is very much a work in progress and as such this README
+will be updated in the near future.
---
+++ src/lib.rs
@@ -0,0 +1,92 @@
+use serde::{de::DeserializeOwned, Serialize};
+use std::collections::HashMap;
+
+#[derive(Clone, Debug)]
+pub struct QueryParams {
+    params: HashMap<String, String>,
+}
+
+impl Default for QueryParams {
+    fn default() -> Self {
+        QueryParams {
+            params: HashMap::new(),
+        }
+    }
+}
+
+impl QueryParams {
+    pub fn new() -> Self {
+        Self::default()
+    }
+
+    pub fn add<K: Into<String>, V: std::fmt::Display>(&mut self, key: K, value: V) {
+        self.params.insert(key.into(), format!("{}", value));
+    }
+}
+
+impl std::fmt::Display for QueryParams {
+    fn fmt(&self, formatter: &mut std::fmt::Formatter<'_>) -> Result<(), std::fmt::Error> {
+        write!(formatter, "?")?;
+        for (key, value) in self.params.iter() {
+            write!(formatter, "{}={}&", key, value)?;
+        }
+
+        Ok(())
+    }
+}
+
+pub enum Body<R> {
+    Valued {
+        value: R,
+    },
+
+    Empty
+}
+
+impl<R> Body<R>
+where
+    R: Serialize,
+{
+    pub fn wrap(value: R) -> Body<R> {
+        Body::Valued { value }
+    }
+
+    pub fn empty() -> Body<R> {
+        Body::Empty
+    }
+}
+
+pub enum HttpError {
+    Undefined,
+}
+
+#[derive(Clone, Debug)]
+pub struct Client {
+    base: String,
+}
+
+impl Client {
+    pub fn new<S: Into<String>>(base: S) -> Client {
+        Client { base: base.into() }
+    }
+
+    pub async fn get<P: AsRef<str>, T: DeserializeOwned>(
+        &mut self,
+        url: P,
+        query_params: QueryParams,
+    ) -> Result<T, HttpError> {
+        let url = format!("{}/{}{}", self.base, url.as_ref(), query_params);
+        surf::get(url.as_str())
+            .recv_json()
+            .await
+            .map_err(|_| HttpError::Undefined)
+    }
+
+    pub async fn post<P: AsRef<str>, T: DeserializeOwned, R: Serialize>(
+        &mut self,
+        body: Body<R>,
+        query_params: QueryParams,
+    ) -> Result<T, HttpError> {
+        todo!()
+    }
+}
